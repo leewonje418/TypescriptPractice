10d18a912333f621385b9467e57d6e2db5f56fdc
leewonje418 <kitti0418@naver.com>
2020-09-15 19:29:35 +0900

    Update dataType.ts

--- dataType.ts
+++ dataType.ts
@@ -31,3 +31,31 @@ enum Color {
 };
 let color: Color = Color.Green;
 console.log(color);
+
+//임의(Any)
+let sure: any = 1;
+sure = "이건 문자열";
+sure = true;
+
+//보이드(void)
+function log(msg): void {
+    console.log("LOG : " + msg);
+}
+
+//null과 undefined
+let a: number = null;
+
+//never(절대 발생하지 않을 값을 의미한다. 예를 들어, 절대 리턴이 발생하지 않는다던가 항상 예외값을 던져서 절대 반환을 하지 않는 경우를 의미한다.)
+function error(message: string): never {
+    throw new Error(message);
+}
+
+function forever(): never {
+    while(true) {
+
+    }
+}
+
+//겍체(Object)
+let user: { name: string; age: number; } = { name: '김철수', age: 12 };
+console.log(user.name);
