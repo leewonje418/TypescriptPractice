f745a7487ca37727698e48a9ad0870f50ca49d1a
leewonje418 <kitti0418@naver.com>
2020-09-23 20:49:28 +0900

    Update Class.ts

--- Class.ts
+++ Class.ts
@@ -34,3 +34,25 @@ class Animal2 {
         return this.name + "가 " + meters + "미터를 움직였다."
     }
 }
+
+class Fish extends Animal2 {
+    constructor(name: string, legs: number = 0) {
+        super(name, legs);
+    }
+}
+
+class Dog extends Animal {
+    constructor(name: string, legs: number = 4) {
+        super(name);
+    }
+    bark(sound: string) {
+        return sound + " " + sound;
+    }
+}
+
+let dog2: Dog = new Dog("멍멍이", 4);
+console.log(dog2.info());
+console.log(dog2.bark('멍!'));
+let fish: Fish = new Fish("니모");
+console.log(fish.move(10));
+
