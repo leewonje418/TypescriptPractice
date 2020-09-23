35b355811bca7a5f0fbf411be6eea7efdb2372a4
leewonje418 <kitti0418@naver.com>
2020-09-23 21:39:04 +0900

    Update limiter.ts

--- limiter.ts
+++ limiter.ts
@@ -16,5 +16,29 @@ class Rect extends Face {
 //private
 class Face2 {
     private edge: number;
-    
+
+    constructor(edge: number) {
+        this.edge
+    }
+
+    public addEdge() {
+        this.edge++;
+    }
+
+    protected getEdge() {
+        return this.edge;
+    }
 }
+
+class Rect2 extends Face {
+    constructor() {
+        super(4);
+    }
+
+    public getEdge() {
+        return this.getEdge()
+    }
+}
+
+const rect = new Rect2();
+console.log(rect.getEdge());
